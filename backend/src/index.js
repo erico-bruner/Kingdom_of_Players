576ffb2f3d8f01c8f5cc8958c094dcfb281c26f0
Erico Bruner <ericodasilvabruner@gmail.com>
2020-04-28 13:55:45 -0300

    Criando base para backend *Pasta src *package.json *yarn.lock *index.js

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+  return response.json({})
+})
+
+app.listen(3333, () => {
+  console.log('>>> Back-end esta no ar <<<')
+})
